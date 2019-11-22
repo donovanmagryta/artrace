cf5c2a652b98ca54254aae863d4ca0c230702b83
Donovan <triops124@gmail.com>
2019-11-21 19:15:24 -0500

    Update overlay.js

--- overlay.js
+++ overlay.js
@@ -1,8 +1,9 @@
 $("input").change(function(e) {
+
     for (var i = 0; i < e.originalEvent.srcElement.files.length; i++) {
-
+        
         var file = e.originalEvent.srcElement.files[i];
-
+        
         var img = document.createElement("img");
         var reader = new FileReader();
         reader.onloadend = function() {
