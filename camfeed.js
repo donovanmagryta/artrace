4d134d0d0d07d39f475242ef6f6f7c0626af0f9c
Donovan <triops124@gmail.com>
2019-11-21 21:29:58 -0500

    Update camfeed.js

--- camfeed.js
+++ camfeed.js
@@ -1,6 +1,6 @@
 // Prefer camera resolution nearest to 1280x720.
-navigator.mediaDevices.getUserMedia()
-.then(function(mediaStream) {
+var constraints = { audio: false, video: { width: 1280, height: 720 } };
+navigator.mediaDevices.getUserMedia(constraints).then(function(mediaStream) {
   var video = document.querySelector('video');
   video.srcObject = mediaStream;
   video.onloadedmetadata = function(e) {
